baa03292ed009102f8cd9f2b2e828112e3b6dae7
Brano Zavracky <branislav.zavracky@brackets.sk>
2021-03-08 23:01:46 +0100

    init

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,55 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import RnShadows from 'rn-shadows';
+import { StyleSheet, Text, View } from 'react-native';
+import Shadow from 'rn-shadows';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    RnShadows.multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Shadow 
+        elevation={4} 
+        shadows={[
+        	[-5, 16, 14,'#17479E', 0.4],
+          [5, 16, 14,'#17479E', 0.4],
+          [-2, 4, 6,'#17479E', 0.5],
+          [2, 4, 6,'#17479E', 0.5]
+        ]}
+      >
+        <View style={{ width: 100, height: 100, backgroundColor: '#444' }} />
+      </Shadow>
+
+      <View style={{ height: 100 }} />
+
+      <Shadow 
+        elevation={4}
+        borderRadius={12} 
+        shadows={[
+          [-5, 16, 14, '#17479E', 0.04],
+          [5, 16, 14, '#17479E', 0.04],
+          [-2, 4, 6, '#17479E', 0.08],
+          [2, 4, 6, '#17479E', 0.08],
+        ]}
+      >
+        <View style={{ width: 300, height: 200, backgroundColor: '#fff' }} />
+      </Shadow>
+
+      <View style={{ height: 100 }} />
+
+      <Shadow 
+        elevation={4}
+        borderRadius={12} 
+        shadows={[
+          [-5, 26, 14, '#333', 0.14],
+          [5, 26, 14, '#333', 0.14],
+          [-2, 4, 6, '#333', 0.18],
+          [2, 4, 6, '#333', 0.18],
+        ]}
+      >
+        <View style={{ backgroundColor: '#fff', padding: 20 }}>
+          <Text>Shadows Works !</Text>
+          <View style={{ width: 60, height: 20, backgroundColor: 'red' }}/>
+        </View>
+      </Shadow>
     </View>
   );
 }
@@ -20,12 +57,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+    paddingVertical: 200,
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,97 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
+import React, { useCallback } from 'react'
+import { Platform, StyleSheet, View } from "react-native";
+
+/**
+ * [width, height, radius, color, opacity]
+ */
+type Shadow = [number, number, number, string, number]
+
+interface ShadowProps {
+  shadows: Shadow[]
+  elevation: number
+  borderRadius?: number
+}
+
+interface ShadowItemsProps {
+  shadow: Shadow
+  zIndex: number
+  borderRadius?: number
+}
+
+const Shadow: React.FC<ShadowProps> = (
+    {
+      children,
+      elevation = 0,
+      shadows,
+      borderRadius = 0
+    }
+  ) => {
+    const renderItem = useCallback(
+      (shadow, index) => (
+        <ShadowItem
+          key={`${shadow.join('-')}${index}`}
+          shadow={shadow}
+          zIndex={index + 2}
+          borderRadius={borderRadius}
+        />
+      ), []
+    );
+
+    return (
+      <View>
+        {Platform.OS === 'ios' && shadows?.map(renderItem)}
+        <View
+          style={{
+            borderRadius,
+            overflow: "hidden",
+            zIndex: shadows?.length + 2 || 0,
+            elevation
+          }}
+        >
+          {children}
+        </View>
+      </View>
+    );
+  }
+
+const buildShadow = (
+  width: number,
+  height: number,
+  radius: number,
+  shadowColor: string,
+  shadowOpacity: number
+) => ({
+  shadowColor,
+  shadowOffset: {
+    height,
+    width,
   },
+  shadowOpacity,
+  shadowRadius: radius,
+});
+
+
+
+const ShadowItem: React.FC<ShadowItemsProps> = ({
+  shadow,
+  zIndex,
+  borderRadius
+}) => {
+  return (
+    <View
+      style={[
+        StyleSheet.absoluteFill,
+        {
+          // backgroundColor needs to be set in order to display shadow
+          backgroundColor: '#fff',
+          flex: 1,
+          borderRadius,
+          zIndex,
+        },
+        buildShadow(...shadow),
+      ]}
+    />
+  );
 };
+
+export default Shadow
