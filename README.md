6eb44e41796e06f4909122e4da98d14530927f67
Brano Zavracky <branislav.zavracky@brackets.sk>
2021-03-10 20:45:03 +0100

    docs: update README

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# rn-shadows
+# `rn-shadows`
 
-shadow component for react native
+Multi layer shadow component for react native
+
+![React Native Bottom Sheet](./example/src/screenshot-ios.png)
 
 ## Installation
+
+Install the library using either Yarn:
+
+```sh
+yarn add rn-shadows
+```
+
+or npm:
 
 ```sh
 npm install rn-shadows
@@ -11,16 +21,25 @@ npm install rn-shadows
 ## Usage
 
 ```js
-import RnShadows from "rn-shadows";
+import Shadow from "rn-shadows";
 
 // ...
 
-const result = await RnShadows.multiply(3, 7);
+<Shadow
+ elevation={6}
+ borderRadius={12}
+ shadows={[
+  [-5, 16, 14, '#17479E', 0.02],
+  [5, 16, 14, '#17479E', 0.02],
+  [-2, 4, 6, '#17479E', 0.03],
+  [2, 4, 6, '#17479E', 0.03],
+ ]}
+>
+ <View style={{ padding: 50 }}>
+    <Text>Shadow</Text>
+ </View>
+</Shadow>
 ```
-
-## Contributing
-
-See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
 
 ## License
 
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,56 +1,44 @@
 import * as React from 'react';
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Shadow from 'rn-shadows';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Shadow 
-        elevation={4} 
-        shadows={[
-        	[-5, 16, 14,'#17479E', 0.4],
-          [5, 16, 14,'#17479E', 0.4],
-          [-2, 4, 6,'#17479E', 0.5],
-          [2, 4, 6,'#17479E', 0.5]
-        ]}
-      >
-        <View style={{ width: 100, height: 100, backgroundColor: '#444' }} />
-      </Shadow>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
 
-      <View style={{ height: 100 }} />
 
-      <Shadow 
-        elevation={4}
-        borderRadius={12} 
-        shadows={[
-          [-5, 16, 14, '#17479E', 0.04],
-          [5, 16, 14, '#17479E', 0.04],
-          [-2, 4, 6, '#17479E', 0.08],
-          [2, 4, 6, '#17479E', 0.08],
-        ]}
-      >
-        <View style={{ width: 300, height: 200, backgroundColor: '#fff' }} />
-      </Shadow>
+        <Shadow 
+          elevation={4}
+          borderRadius={12} 
+          shadows={[
+            [-5, 16, 14, '#17479E', 0.04],
+            [5, 16, 14, '#17479E', 0.04],
+            [-2, 4, 6, '#17479E', 0.08],
+            [2, 4, 6, '#17479E', 0.08],
+          ]}
+        >
+          <View style={{ width: 300, height: 200, backgroundColor: '#fff' }} />
+        </Shadow>
 
-      <View style={{ height: 100 }} />
+        <View style={{ height: 100 }} />
+        
+        <Shadow 
+          elevation={4} 
+          borderRadius={200}
+          shadows={[
+            [-5, -10, 14,'#999', 0.2],
+            [5, 15, 14,'#999', 0.2],
+            [-2, 4, 6,'#999', 0.2],
+            [2, 4, 6,'#999', 0.2]
+          ]}
+        >
+          <View style={{ width: 200, height: 200, backgroundColor: '#fff' }} />
+        </Shadow>
 
-      <Shadow 
-        elevation={4}
-        borderRadius={12} 
-        shadows={[
-          [-5, 26, 14, '#333', 0.14],
-          [5, 26, 14, '#333', 0.14],
-          [-2, 4, 6, '#333', 0.18],
-          [2, 4, 6, '#333', 0.18],
-        ]}
-      >
-        <View style={{ backgroundColor: '#fff', padding: 20 }}>
-          <Text>Shadows Works !</Text>
-          <View style={{ width: 60, height: 20, backgroundColor: 'red' }}/>
-        </View>
-      </Shadow>
-    </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -60,6 +48,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical: 200,
+    // paddingVertical: 200,
   },
 });
